b6ca35c8bd93492c58f7b463bfcd3a324eea3e4b
James Brink <brink.james@gmail.com>
2025-04-17 01:10:29 -0700

    Initial commit: ComfyUI Nix flake setup with Python 3.12

---
+++ README.md
@@ -0,0 +1,56 @@
+# ComfyUI Nix Flake
+
+A Nix flake for installing and running [ComfyUI](https://github.com/comfyanonymous/ComfyUI) with Python 3.12.
+
+## Features
+
+- Provides ComfyUI packaged with Python 3.12
+- Reproducible environment through Nix flakes
+- Development shell for contributing or customizing
+
+## Usage
+
+### Running ComfyUI
+
+```bash
+# Run directly from the flake
+nix run
+
+# Or if you've cloned the repository
+nix run .#
+```
+
+### Development Shell
+
+```bash
+# Enter a development shell with all dependencies
+nix develop
+```
+
+### Installation
+
+You can install ComfyUI to your profile:
+
+```bash
+nix profile install github:jamesbrink/comfy-ui
+```
+
+## Customization
+
+The flake is designed to be simple and extensible. You can customize it by:
+
+1. Adding additional Python packages in the `comfyEnv` definition
+2. Modifying runtime parameters in the `installPhase`
+3. Pinning to a specific ComfyUI version by changing the `rev` in `fetchFromGitHub`
+
+## First-time Setup
+
+When first building the flake, you'll need to replace the `fakeSha256` with the actual hash:
+
+1. Run `nix build` and it will fail with the correct hash
+2. Replace `pkgs.lib.fakeSha256` with the provided hash
+3. Run `nix build` again to complete the build
+
+## License
+
+This flake is provided under the MIT license. ComfyUI itself is licensed under GPL-3.0.
---
+++ flake.nix
@@ -0,0 +1,128 @@
+{
+  description = "A Nix flake for ComfyUI with Python 3.12";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # Allow unfree packages
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+
+        python = pkgs.python312;
+        
+        # Define the Python packages we need
+        pythonPackages = ps: with ps; [
+          # Core dependencies
+          pip
+          setuptools
+          wheel
+          
+          # Basic dependencies
+          numpy
+          pillow
+          pyyaml
+          requests
+          tqdm
+          aiohttp
+          scipy
+          psutil
+        ];
+        
+        # Create a Python environment with the base packages
+        baseEnv = python.withPackages pythonPackages;
+        
+      in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "comfy-ui";
+          version = "0.1.0";
+          
+          src = pkgs.fetchFromGitHub {
+            owner = "comfyanonymous";
+            repo = "ComfyUI";
+            rev = "master"; # Consider pinning to a specific commit/tag for reproducibility
+            sha256 = "sha256-QTik5CjpvZsVwQtHkKVOV2D9QpwCtZcJipgPZbJGwFo=";
+          };
+          
+          nativeBuildInputs = [
+            pkgs.makeWrapper
+          ];
+          
+          buildInputs = [
+            baseEnv
+            # Add ML packages separately to avoid collisions
+            python.pkgs.torch-bin
+            python.pkgs.torchvision
+            python.pkgs.safetensors
+            python.pkgs.transformers
+            python.pkgs.opencv4
+          ];
+          
+          # Skip build phase as ComfyUI is a Python application that doesn't need building
+          dontBuild = true;
+          
+          # Skip configure phase
+          dontConfigure = true;
+          
+          installPhase = ''
+            # Create directories
+            mkdir -p $out/bin
+            mkdir -p $out/share/comfy-ui
+            
+            # Copy ComfyUI files
+            cp -r $src/* $out/share/comfy-ui/
+            
+            # Create wrapper script with proper PYTHONPATH
+            makeWrapper ${python}/bin/python $out/bin/comfy-ui \
+              --prefix PYTHONPATH : "${baseEnv}/${python.sitePackages}" \
+              --prefix PYTHONPATH : "${python.pkgs.torch-bin}/${python.sitePackages}" \
+              --prefix PYTHONPATH : "${python.pkgs.torchvision}/${python.sitePackages}" \
+              --prefix PYTHONPATH : "${python.pkgs.safetensors}/${python.sitePackages}" \
+              --prefix PYTHONPATH : "${python.pkgs.transformers}/${python.sitePackages}" \
+              --prefix PYTHONPATH : "${python.pkgs.opencv4}/${python.sitePackages}" \
+              --prefix PYTHONPATH : "$out/share/comfy-ui" \
+              --run "cd $out/share/comfy-ui" \
+              --add-flags "main.py"
+          '';
+          
+          meta = with pkgs.lib; {
+            description = "ComfyUI with Python 3.12";
+            homepage = "https://github.com/comfyanonymous/ComfyUI";
+            license = licenses.gpl3;
+            platforms = platforms.all;
+            mainProgram = "comfy-ui";
+          };
+        };
+        
+        apps.default = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.default;
+          name = "comfy-ui";
+        };
+        
+        devShells.default = pkgs.mkShell {
+          packages = [
+            baseEnv
+            python.pkgs.torch-bin
+            python.pkgs.torchvision
+            python.pkgs.safetensors
+            python.pkgs.transformers
+            python.pkgs.opencv4
+          ];
+          
+          shellHook = ''
+            echo "ComfyUI development environment activated"
+            echo "Run 'python main.py' to start ComfyUI"
+            export PYTHONPATH="$PWD:$PYTHONPATH"
+          '';
+        };
+      }
+    );
+}
